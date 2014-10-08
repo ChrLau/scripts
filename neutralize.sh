817478d0155bd55b829e66c6c21012a660509bf7
ChrLau <ChrLau@users.noreply.github.com>
2014-10-08 12:30:32 +0200

    Create neutralize.sh

---
+++ neutralize.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# neutralize.sh
+# AUTHOR: Christian Lauf
+# This script is useful is you want to "neutralize" some script. Ensuring that it can't be executed/changed.
+# Warning: As this script modifies timestamps and filerights it shouldn't be used as a forensic tool.
+
+CHMOD=/bin/chmod
+CHOWN=/bin/chown
+CHATTR=/usr/bin/chattr
+LSATTR=/usr/bin/lsattr
+STAT=/usr/bin/stat
+
+if [ "$#" != "1" ]; then
+        echo "Usage: $0 /path/to/file"
+        exit 1
+fi
+
+if [ -f "$1" ]; then
+
+  $CHOWN 0000 $1
+  $CHOWN root:root $1
+
+  echo -e "\e[0;32mMade file 0000, root:root and immutable bit set:\e[0m"
+  $STAT $1
+  $CHATTR +i $1
+
+  echo -e "\e[0;32mSet immutable bit on file:\e[0m"
+  $LSATTR $1
+
+else
+
+  echo -e "\e[0;31m $1 is not a regular file.\e[0m"
+  exit 2
+
+fi
