003f268b67e1ed59a59c97007762b1f8a7b957c9
ChrLau <ChrLau@users.noreply.github.com>
2014-10-09 16:57:50 +0200

    Create keepalive-diff.sh

---
+++ keepalive-diff.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# keepalie-diff.sh
+# This script executes a "ipvsadm -L -n --sort | cut -c 1-40" and both nodes of a clustered loadbalancer-pair
+# Printing out the differences
+# Useful for first troubleshooting or when doing config rollouts
+
+# Set this to the username you wish to use:
+sshuser = $USER
+
+if [ "$#" != "1" ]; then
+  echo "Usage: $0 hostname (without node-identifier)\nExample: $0 ipkeepalive05 (for diffing ipkeepalive05a and ipkeepalive05b)" 
+  exit 1
+fi
+
+echo "Diffing: $1"
+echo 
+diff -B -y --suppress-common-lines <(ssh $sshuser@${1}a ipvsadm -L -n --sort | cut -c 1-40) <(ssh $sshuser@${1}b ipvsadm -L -n --sort | cut -c 1-40)
