373540fbe09b0c60a1ef764e8bb57374668a5b5c
Christian Lauf <ChrLau@users.noreply.github.com>
2014-10-15 11:44:53 +0200

    Update keepalive-diff.sh
    Added user option, introduced HOST-variable for easier code-reading

--- keepalive-diff.sh
+++ keepalive-diff.sh
@@ -5,15 +5,23 @@
 # Printing out the differences
 # Useful for first troubleshooting or when doing config rollouts
 
-# Set this to the username you wish to use:
-# Default is executing user
-SSHUSER=$USER
+HOST=$1
+
+if [ -n "$2" ]; then
+  # Set this to the username you wish to use:
+  SSHUSER=$2
+else
+  # Default is executing user
+  SSHUSER=$USER
+fi
+
+
 
 if [ "$#" != "1" ]; then
-  echo -e "Usage: $0 hostname (without node-identifier)\nExample: $0 ipkeepalive05 (for diffing ipkeepalive05a and ipkeepalive05b)" 
+  echo -e "Usage: $0 hostname [user]\nExample: $0 ipkeepalive05 (for diffing ipkeepalive05a and ipkeepalive05b)\nUsername is optional." 
   exit 1
 fi
 
 echo -e "Diffing: $1\n"
 
-diff -B -y --suppress-common-lines <(ssh "$SSHUSER"@"${1}a" ipvsadm -L -n --sort | cut -c 1-40) <(ssh "$SSHUSER"@"${1}b" ipvsadm -L -n --sort | cut -c 1-40)
+diff -B -y --suppress-common-lines <(ssh "$SSHUSER@${HOST}a" ipvsadm -L -n --sort | cut -c 1-40) <(ssh "$SSHUSER@${HOST}b" ipvsadm -L -n --sort | cut -c 1-40)
