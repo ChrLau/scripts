74786afd6fbc2b1c2c615009f2709dc7fbc1902c
Christian Lauf <ChrLau@users.noreply.github.com>
2021-03-05 14:46:54 +0100

    Update keepalived-state.sh

--- keepalived-state.sh
+++ keepalived-state.sh
@@ -11,27 +11,59 @@ keepalivedps=$(pgrep -u root -f "/usr/sbin/keepalived" -c)
 keepalivedconf="/etc/keepalived/keepalived.conf"
 # Set checkifup to be NOT 0 or 1 so the if statement will fail if it's not set
 checkifup=3
+state=""
+
+# Check for optional command line parameter
+if [[ -n "$1" ]]; then
+
+  # Enforce precise naming
+  if [[ "$1" == "MASTER" ]] || [[ "$1" == "BACKUP" ]]; then
+    state="$1"
+  else
+    echo "Valid parameters (case-sensitive) are: MASTER or BACKUP"
+    echo "Note: If a parameter is given script will exit with 0 if given state matches keepaliveds running state."
+    echo "      Otherwise it will exit with 1."
+    echo "      If no parameter is given it will echo the state."
+    exit 1
+  fi
+
+fi
+
 
 if [[ "$keepalivedps" -gt 1 ]]; then
 
   if [[ -r $keepalivedconf ]]; then
-    virtualip=$(grep 'virtual_ipaddress {' -A 10 $keepalivedconf | grep -B 500 -m1 "}" | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
-
-    if [[ "$virtualip" -eq 1 ]]; then
+    virtualip=$(grep 'virtual_ipaddress {' -A 10 $keepalivedconf | grep -B 500 -m1 "}" | grep -Eo '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)' )
+    virtualipnr=$(echo -n "$virtualip" | grep -c '^')
+    
+    if [[ "$virtualipnr" -eq 1 ]]; then
       checkifup=$(ip a |grep -cPo "$virtualip/")
 
       if [[ "$checkifup" -eq 1 ]]; then
         role=MASTER
-        echo "Role: $role"
       elif [[ "$checkifup" -eq 0 ]]; then
         role=BACKUP
-        echo "Role: $role"
+
+        if [[ -n "$state" ]]; then
+
+          if [[ "$state" == "$role" ]]; then
+            echo "Given state matches running state"
+            exit 0
+          else
+            echo "Given state does not match running state"
+            exit 1
+          fi
+
+        else
+          echo "Role: $role"
+        fi
+
       else
         echo "Couldn't check if node is MASTER or BACKUP (checkifup is not 0 or 1). Aborting."
         exit 1
       fi
 
-    elif [[ "$virtualip" -gt 1 ]]; then
+    elif [[ "$virtualipnr" -gt 1 ]]; then
       echo "More than 1 virtual_ipaddress found in $keepalivedconf. Script currently can't safely handle this. Aborting."
       exit 1
     else
