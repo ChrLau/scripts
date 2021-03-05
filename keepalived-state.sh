b31202866344627a82643cb4d796128e79fe7b5d
Christian Lauf <ChrLau@users.noreply.github.com>
2021-03-05 13:53:21 +0100

    Update keepalived-state.sh

--- keepalived-state.sh
+++ keepalived-state.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Checks if keepalived has the MASTER or BACKUP state
-# Note: Currently assumes there is only 1 virtual_ipaddress block in the whole config with only 1 IPv4 IP
-
 keepalivedps=$(pgrep -u root -f "/usr/sbin/keepalived" -c)
 keepalivedconf="/etc/keepalived/keepalived.conf"
+# Set checkifup to be NOT 0 or 1 so the if statement will fail if it's not set
+checkifup=3
 
-if [ "$keepalivedps" -gt 1 ]; then
+if [[ "$keepalivedps" -gt 1 ]]; then
 
-  if [[ -f $keepalivedconf ]]; then
+  if [[ -r $keepalivedconf ]]; then
     virtualip=$(grep 'virtual_ipaddress {' -A 10 $keepalivedconf | grep -B 500 -m1 "}" | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
 
-    if [[ -n "$virtualip" ]]; then
+    if [[ "$virtualip" -eq 1 ]]; then
       checkifup=$(ip a |grep -cPo "$virtualip/")
 
       if [[ "$checkifup" -eq 1 ]]; then
@@ -25,13 +24,16 @@ if [ "$keepalivedps" -gt 1 ]; then
         exit 1
       fi
 
+    elif [[ "$virtualip" -gt 1 ]]; then
+      echo "More than 1 virtual_ipaddress found in $keepalivedconf. Script currently can't safely handle this. Aborting."
+      exit 1
     else
       echo "virtual_ipaddress not found in /etc/keepalived/keepalived.conf. Aborting."
       exit 1
     fi
 
   else
-    echo "/etc/keepalived/keepalived.conf not readable. Aborting."
+    echo "$keepalivedconf not readable. Aborting."
     exit 1
   fi
 
