77d2da9d6da7d14045d3f38921aaf14cdabd2d0b
Christian Lauf <github.com@christian-lauf.info>
2023-10-19 16:34:33 +0200

    Update get-windows-wifi-pws.bat

--- get-windows-wifi-pws.bat
+++ get-windows-wifi-pws.bat
@@ -1,11 +1,11 @@
 REM Batch-Script to print all stored WLANs and their passwords
 REM Useful if you don't remember the password but need to add another device
 
-REM Execute in cmd
+REM Execute in cmd with admin rights
 @echo off
 setlocal enabledelayedexpansion
 
-for /F "tokens=2 delims=:" %%a in (`netsh wlan show profile`) do (
+for /F "tokens=2 delims=:" %%a in ('netsh wlan show profile') do (
   set wifi_password=
   for /F "tokens=2 delims=: usebackq" %%F IN (`netsh wlan show profile %%a key^=clear ^|find "Key Content"`) do (
     set wifi_password=%%F
