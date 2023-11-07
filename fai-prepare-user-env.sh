7eedfe9e908ef9f9b3652d31f101ce13e0f118b5
Christian Lauf <github.com@christian-lauf.info>
2023-11-07 22:34:50 +0100

    Add networkconfig part

--- fai-prepare-user-env.sh
+++ fai-prepare-user-env.sh
@@ -140,10 +140,57 @@ mkdir /home/clauf/.ssh && chmod 0700 /home/clauf/.ssh && cp /root/.ssh/authorize
 # Update package lists
 apt-get update
 
-# TODO:
-# 1. Get next free IP (from where?)
-#  - Ping-loop through defined address space
-#  - First IP which doesn't reply: Take this IP
-#  - Get Hostname from dig -t a IP.IP.IP.IP @internaldns
-# 2. Make network configuration static (at least for IPv4)
-# 3. Set hostname according to DNS (/etc/hosts & /etc/hostname)
+# Generate static IPv4 network config
+NMAP="$(which nmap)"
+# Test if ssh is present and executeable
+if [ ! -x "$NMAP" ]; then
+  echo "This script requires nmap. Exiting."
+  exit 2;
+fi
+
+FIRST_DOWN_HOST=$(nmap -v -sn -R 192.168.178.20-49 -oG - | grep -m1 -oP "^Host:[[:space:]]192\.168\.178\.[0-9]{2}[[:space:]]\([a-zA-Z0-9\-]+\.lan\)[[:space:]]Status: Down")
+#echo "FIRST_DOWN_HOST: $FIRST_DOWN_HOST"
+
+IP=$(awk '{print $2}' <<<$FIRST_DOWN_HOST)
+#echo "IP: $IP"
+
+FQDN=$(awk -F'[()]' '{print $2}' <<<$FIRST_DOWN_HOST)
+#echo "FQDN: $FQDN"
+
+GATEWAY=$(ip -o -4 route show to default | awk '{print $3}')
+#echo "GATEWAY: $GATEWAY"
+
+INTERFACE=$(ip -o -4 route show to default | awk '{print $5}')
+#echo "INTERFACE: $INTERFACE"
+
+# Save old network config
+if [ -f /etc/network/interfaces ]; then
+  cp /etc/network/interfaces /etc/network/interfaces.scriptcopy
+fi
+
+# Write new config
+cat <<NETWORKEOF > /etc/network/interfaces
+# interfaces(5) file used by ifup(8) and ifdown(8)
+# Include files from /etc/network/interfaces.d:
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+allow-hotplug $INTERFACE
+iface $INTERFACE inet static
+  address $IP
+  netmask 255.255.255.0
+  gateway $GATEWAY
+  dns-nameservers 192.168.178.9
+
+#iface $INTERFACE inet6 static
+#  address fd00::b4fd:51ff:fe7b:XXXX
+#  netmask 64
+#  gateway fe80::1
+NETWORKEOF
+
+#TODO:
+# /etc/hosts & /etc/hostname
