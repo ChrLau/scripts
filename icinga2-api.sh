87130dde9045834d758b4012cffd01a02ff8f560
Christian Lauf <github.com@christian-lauf.info>
2023-12-15 11:33:45 +0100

    Version 1.1, add Version, add source comment, fix shellcheck

--- icinga2-api.sh
+++ icinga2-api.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # vim: set tabstop=2 smarttab shiftwidth=2 softtabstop=2 expandtab foldmethod=syntax :
 #
-#
+# Source can be found here:
+# https://github.com/ChrLau/scripts/blob/master/icinga2-api.sh
 ICINGA2_API_USER=""
 ICINGA2_API_PASSWORD=""
 ICINGA2_API_HOST=ip.ip.ip.ip
 ICINGA2_API_PORT=
 
+VERSION="1.1"
 SCRIPT="$(basename "$0")"
 JSON_PP="$(command -v json_pp)"
 CURL="$(command -v curl)"
@@ -24,7 +26,7 @@ if [ ! -x "$JSON_PP" ]; then
 fi
 
 function HELP {
-  echo "$SCRIPT: Query the Icinga2-API for latest check results"
+  echo "$SCRIPT v$VERSION): Query the Icinga2-API for latest check results"
   echo "Usage: $SCRIPT FQDN Servicename"
   echo ""
   echo "Both FQDN & Servicename can specified as RegEx."
@@ -32,9 +34,9 @@ function HELP {
 }
 
 # For later version where it's possible to specify what results to retrieve from the API
-ONLY_ERRORS="&& !match(\"0\",service.state)"
-HOSTFILTER="\"filter\": \"match(\"'$HOST'\",host.name)"
-SERVICEFILTER="\"filter\": \"regex(pattern, service.name)\", \"filter_vars\": { \"pattern\": \"'$SERVICENAME'\" }"
+#ONLY_ERRORS="&& !match(\"0\",service.state)"
+#HOSTFILTER="\"filter\": \"match(\"'$HOST'\",host.name)"
+#SERVICEFILTER="\"filter\": \"regex(pattern, service.name)\", \"filter_vars\": { \"pattern\": \"'$SERVICENAME'\" }"
 
 # Print help if no arguments are given
 if [ "$#" -eq 0 ]; then
@@ -44,9 +46,9 @@ if [ "$#" -eq 0 ]; then
 elif [ "$#" -eq 1 ]; then
 
   HOST="$1"
-  results=$($CURL -s -u $ICINGA2_API_USER:$ICINGA2_API_PASSWORD -H 'Accept: application/json' -H 'X-HTTP-Method-Override: GET' -X POST -k "https://$ICINGA2_API_HOST:$ICINGA2_API_PORT/v1/objects/services/" -d '{"filter": "match(\"'"$HOST"'\",host.name)", "attrs": ["__name", "state", "action_url", "last_check_result"] }')
+  results=$($CURL -s -u "$ICINGA2_API_USER":"$ICINGA2_API_PASSWORD" -H 'Accept: application/json' -H 'X-HTTP-Method-Override: GET' -X POST -k "https://$ICINGA2_API_HOST:$ICINGA2_API_PORT/v1/objects/services/" -d '{"filter": "match(\"'"$HOST"'\",host.name)", "attrs": ["__name", "state", "action_url", "last_check_result"] }')
   # Get services based on service name
-  #results=$($CURL -s -u $ICINGA2_API_USER:$ICINGA2_API_PASSWORD -H 'Accept: application/json' -H 'X-HTTP-Method-Override: GET' -X POST -k "https://$ICINGA2_API_HOST:$ICINGA2_API_PORT/v1/objects/services/" -d '{"filter": "match(\"'"$HOST"'\",service.name)", "attrs": ["__name", "state", "action_url", "last_check_result"] }')
+  #results=$($CURL -s -u "$ICINGA2_API_USER":"$ICINGA2_API_PASSWORD" -H 'Accept: application/json' -H 'X-HTTP-Method-Override: GET' -X POST -k "https://$ICINGA2_API_HOST:$ICINGA2_API_PORT/v1/objects/services/" -d '{"filter": "match(\"'"$HOST"'\",service.name)", "attrs": ["__name", "state", "action_url", "last_check_result"] }')
 
   echo "$results" | "$JSON_PP"
 
@@ -54,7 +56,7 @@ elif [ "$#" -eq "2" ]; then
 
   HOST="$1"
   SERVICENAME="$2"
-  results=$($CURL -s -u $ICINGA2_API_USER:$ICINGA2_API_PASSWORD -H 'Accept: application/json' -H 'X-HTTP-Method-Override: GET' -X POST -k "https://$ICINGA2_API_HOST:$ICINGA2_API_PORT/v1/objects/services/" -d '{ "filter": "regex(\"'"$HOST"'\",host.name) && regex(\"'"$SERVICENAME"'\",service.name)", "attrs": ["__name", "state", "action_url", "last_check_result"] }')
+  results=$($CURL -s -u "$ICINGA2_API_USER":"$ICINGA2_API_PASSWORD" -H 'Accept: application/json' -H 'X-HTTP-Method-Override: GET' -X POST -k "https://$ICINGA2_API_HOST:$ICINGA2_API_PORT/v1/objects/services/" -d '{ "filter": "regex(\"'"$HOST"'\",host.name) && regex(\"'"$SERVICENAME"'\",service.name)", "attrs": ["__name", "state", "action_url", "last_check_result"] }')
 
   echo "$results" | "$JSON_PP"
 
