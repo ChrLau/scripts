e138fa69ba01e2d309874ad5d06addd1b8269593
Christian Lauf <github-ssh@christian-lauf.info>
2024-08-15 21:39:57 +0200

    Only press button when the screen isnt locked

--- no-screenlock-during-meeting.ps1
+++ no-screenlock-during-meeting.ps1
@@ -1,3 +1,15 @@
+# If the computer isn't locked:
 # Press the Scrolllock-Key every 280 seconds to prevent the automatic screen locking
 $WShell = New-Object -Com "Wscript.shell"
-while (1) {$WShell.SendKeys("{SCROLLLOCK}"); sleep 280}
+while (1) {
+  # Check if the logonui process is running - which is only the case when the Lockscreen is up
+  if ( Get-Process logonui -ErrorAction SilentlyContinue ) {
+    # Computer is locked, do nothing and check again
+    sleep 280;
+  } else {
+    # Computer is unlocked
+    $WShell.SendKeys("{SCROLLLOCK}");
+    sleep 280;
+  }
+}
+
